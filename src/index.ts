40bc2f7d0b738f64e433c99fdfb6f085204d35e4
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2023-12-13 12:05:42 +0800

    update init cli app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-console.log('started')
+import main from './main'
+main()
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import Config from '@kagiweb-tech/api-core-a/utils/config'
+
+// 1. administrative commands
+//      ---- generators ----
+//      a. generate secret key
+//      b. hash a password
+//      ---- DB ----
+//      c. clean database
+//      d. seed database
+//      e. reset database
+
+// 2. app creator
+//      a. Server (using kagiweb-api-core-a)
+//      b. Webapp (using kagiweb-ui-core-a)
+
+export default () => {
+    console.log('cli started');
+    const env = Config.getEnv()
+    console.log('config env: ', env) 
+}
